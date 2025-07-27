3f935af65b3dbf5d2fad6dc63032227ede2ccb71
jayasree1012 <sailaja.jash@gmail.com>
2025-07-27 10:51:52 +0530

    Add files via upload

---
+++ Day-1/NumbersUsingLoop.java
@@ -0,0 +1,25 @@
+public class NumbersUsingLoop
+{
+	public static void main(String[] args) {
+	    Integer num = 5678;
+	    int temp = num;
+	   // String numStr = num.toString();
+	   //int count = numStr.length();
+	   int count = 0;
+	   while(num > 0){
+	       num = num / 10;
+	       count++;
+	   }
+	   num = temp;
+	   //System.out.println("Count: "+ count);
+	   //int power = 1;
+	   int total = 0;
+	   while(num > 0){
+	       int a = num%10;
+	       total += Math.pow(a, count--);
+	       //System.out.println(total);
+	       num = num / 10;
+	   }
+	   System.out.print(total);
+	}
+}
