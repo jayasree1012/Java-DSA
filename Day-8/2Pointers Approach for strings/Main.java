1131c1d5051dc27e9552c96ca075fc73b1444a3e
jayasree1012 <sailaja.jash@gmail.com>
2025-07-27 14:43:18 +0530

    Create Main.java

---
+++ Day-8/2Pointers Approach for strings/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		char[] reverse=str.toCharArray();
+		int left=0;
+		int right=str.length()-1;
+		while(left<right) {
+			char temp=reverse[left];
+			reverse[left]=reverse[right];
+			reverse[right]=temp;
+			left++;
+			right--;
+		}
+		String result=new String(reverse);
+		System.out.print(result);
+
+
+	}
+}
