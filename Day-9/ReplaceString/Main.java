c8d2b006eb76d4112df0988c10c1127afc941f4a
jayasree1012 <sailaja.jash@gmail.com>
2025-07-27 14:58:14 +0530

    Create Main.java

---
+++ Day-9/ReplaceString/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class Main {
+	public static void main(String[] args) {
+		String s = "bcabcabc";
+		String result=s.replaceAll("ab","");
+		System.out.println(result);
+
+	}
+}
